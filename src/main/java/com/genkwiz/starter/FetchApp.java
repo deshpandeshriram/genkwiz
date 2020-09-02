f66f6dd9160ff5085e41e9856d5786ce5b22a71b
vrushbru <47853078+vrushbru@users.noreply.github.com>
2020-09-02 20:20:04 +0530

    Add files via upload

---
+++ src/main/java/com/genkwiz/starter/FetchApp.java
@@ -0,0 +1,35 @@
+package com.genkwiz.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class FetchApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(FetchApp.class, args);
+	}
+
+	
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/questions/*")).
+				apis(RequestHandlerSelectors.basePackage("com.genkwiz"))
+				.build();
+				
+		
+	}
+}
+	
