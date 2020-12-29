586900605f0dc5e37811333c26aba549a0fee809
vrushbru <47853078+vrushbru@users.noreply.github.com>
2020-12-29 15:22:46 +0530

    Update DB_Script.sql

--- DB_Script.sql
+++ DB_Script.sql
@@ -1,7 +1,7 @@
-//Database for genkwiz.com
+/*Database for genkwiz.com*/
 create database genkwiz;
 
-//Purpose:Consists of information about the various modes provided in the application.
+/*Purpose:Consists of information about the various modes provided in the application.*/
 create table question_mode(
 id bigserial not null,
 mode_name varchar not null,
@@ -10,7 +10,7 @@ max_score int not null,
 primary key (id)
 );
 
-//Purpose:Consists of all the questions available for the multiple types of quizzes.
+/*Purpose:Consists of all the questions available for the multiple types of quizzes.*/
 create table question_bank(
 id bigserial not null  ,
 question text not null,
@@ -24,7 +24,7 @@ foreign key(mode_id) references question_mode(id)
 
 create index index_question_bank on question_bank (genre , mode_id);
 
-//Purpose:Consists of all information about the hints available for individual questions.
+/*Purpose:Consists of all information about the hints available for individual questions.*/
 create table question_hint(
 id serial not null  ,
 question_id int not null,
@@ -39,7 +39,7 @@ foreign key (question_id) references question_Bank(id)
 
 create index index_question_hint on question_hint (question_id , number);
 
-//Purpose:Consists of all information about the multimedia available for individual questions.
+/*Purpose:Consists of all information about the multimedia available for individual questions.*/
 create table multimedia(
 id serial not null    ,
 question_id int not null,
@@ -52,7 +52,7 @@ foreign key (question_id) references question_bank(id)
 
 create index index_multimedia on multimedia (question_id , number);
 
-//Purpose:Consists of a set of correct answers for subjective questions.
+/*Purpose:Consists of a set of correct answers for subjective questions.*/
 create table subjective_answer(
 id bigserial not null,
 question_id int not null,
@@ -63,7 +63,7 @@ foreign key (question_id) references question_bank(id)
 
 create index index_subjective_answer on subjective_answer (question_id , answer_value);
 
-//Purpose:Consists of a set of correct answers for multiple choice questions.
+/*Purpose:Consists of a set of correct answers for multiple choice questions.*/
 create table mcq_answer(
 id bigserial not null   ,
 question_id int not null,
@@ -76,7 +76,7 @@ foreign key (question_id) references question_bank(id)
 
 create index index_mcq_answer on mcq_answer (question_id , number);
 
-//Purpose: Consists information about quizzes to be generated.
+/*Purpose: Consists information about quizzes to be generated.*/
 create table quiz(
 id int not null,
 question_id int not null,
@@ -88,7 +88,7 @@ is_attempted varchar (6) not null
 
 create index index_quiz on quiz (question_id , score_per_question);
 
-//Purpose: Consists information about performance parameters to evaluate users.
+/*Purpose: Consists information about performance parameters to evaluate users.*/
 create table performance(
 id serial not null   ,
 user_id varchar not null,
