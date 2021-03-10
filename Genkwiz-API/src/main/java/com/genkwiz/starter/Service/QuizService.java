3c2f676eea46022c8faa6b6a61003719489e0b05
vrushbru <47853078+vrushbru@users.noreply.github.com>
2021-03-10 19:27:31 +0530

    Update QuizService.java

--- Genkwiz-API/src/main/java/com/genkwiz/starter/Service/QuizService.java
+++ Genkwiz-API/src/main/java/com/genkwiz/starter/Service/QuizService.java
@@ -1,11 +1,13 @@
 package com.genkwiz.starter.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.genkwiz.starter.Entity.Quiz;
+import com.genkwiz.starter.Repository.QuestionRepository;
 import com.genkwiz.starter.Repository.QuizRepository;
 
 @Service
@@ -13,9 +15,23 @@ public class QuizService {
 
 		@Autowired
 		QuizRepository quizRepository;
-
+		@Autowired
+		private QuestionRepository questionRepo;
+		
 		public List<Quiz> getQuiz(int quizId) {
 
 			return quizRepository.findByQuizId(quizId);
+		}
+		
+		public int createQuiz(String genre, int mode, int noOfQuestions) {
+			
+			
+			return quizRepository.createQuiz(genre,mode,noOfQuestions);
+			
+		}
+ 
+		public void createQuiz() {
+			// TODO Auto-generated method stub
+			 quizRepository.createQuiz();
 		}
 }
