69930b3e677cfbd9bef7ca7f6ec5b4337b578003
vrushbru <47853078+vrushbru@users.noreply.github.com>
2021-03-30 22:26:20 +0530

    Update QuizService.java

--- Genkwiz-API/src/main/java/com/genkwiz/starter/Service/QuizService.java
+++ Genkwiz-API/src/main/java/com/genkwiz/starter/Service/QuizService.java
@@ -1,11 +1,18 @@
 package com.genkwiz.starter.Service;
 
+import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+
+import javax.persistence.PrePersist;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
+import com.genkwiz.starter.Entity.Question;
 import com.genkwiz.starter.Entity.Quiz;
 import com.genkwiz.starter.Repository.QuestionRepository;
 import com.genkwiz.starter.Repository.QuizRepository;
@@ -15,23 +22,53 @@ public class QuizService {
 
 		@Autowired
 		QuizRepository quizRepository;
-		@Autowired
-		private QuestionRepository questionRepo;
+		/*@Autowired
+		private QuestionRepository questionRepo;*/
 		
-		public List<Quiz> getQuiz(int quizId) {
+		public List<Quiz> getQuiz(UUID quizId) {
 
 			return quizRepository.findByQuizId(quizId);
 		}
 		
 		public int createQuiz(String genre, int mode, int noOfQuestions) {
+			UUID uniqueId=UUID.randomUUID();
+			
+			/*List<Question> allQuestions,randomQuestions = new ArrayList<>();
+			allQuestions=questionRepo.findByQGenreAndModeId(genre,mode);
+			//Quiz quiz1=new Quiz();
 			
 			
-			return quizRepository.createQuiz(genre,mode,noOfQuestions);
+			SecureRandom rand = new SecureRandom();
+			    for (int i = 0; i < Math.min(noOfQuestions, allQuestions.size()); i++) {
+			        randomQuestions.add( allQuestions.remove( rand.nextInt( allQuestions.size() ) ));
+			    }
+			    
+			List<Integer> questionIds=randomQuestions.stream().map(Question::getqId).collect(Collectors.toList());  
 			
-		}
- 
-		public void createQuiz() {
-			// TODO Auto-generated method stub
-			 quizRepository.createQuiz();
-		}
+			 List<Quiz> quiz = new ArrayList<>();
+
+			    for (int i = 0; i < noOfQuestions; i++) {
+			        Quiz question = new Quiz();
+			        question.setQuizId(uniqueId);
+			        question.setqId(questionIds.get(i));
+			       
+
+			        quiz.add(question);
+			    }
+
+
+			    quizRepository.save(quiz);
+			    return ((Quiz) quiz).getQuizId();*/
+			
+			
+		    
+			return quizRepository.createQuiz(genre,mode,noOfQuestions,uniqueId);
+			}
+	
+	    
 }
+			
+ 
+
+		
+
