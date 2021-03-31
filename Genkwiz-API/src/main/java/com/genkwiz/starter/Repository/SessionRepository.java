13c6d3421004afec4ae4f55eea04ba6663c77be8
vrushbru <47853078+vrushbru@users.noreply.github.com>
2021-03-31 23:29:50 +0530

    Add files via upload

---
+++ Genkwiz-API/src/main/java/com/genkwiz/starter/Repository/SessionRepository.java
@@ -0,0 +1,15 @@
+package com.genkwiz.starter.Repository;
+
+import java.util.UUID;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.genkwiz.starter.Entity.SessionManagement;
+
+public interface SessionRepository extends CrudRepository<SessionManagement,UUID>{
+
+	
+	
+
+		
+}
