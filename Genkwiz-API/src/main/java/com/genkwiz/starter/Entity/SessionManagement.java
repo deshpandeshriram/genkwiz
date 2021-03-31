d21f3266286f9bd84cf2c254c9ba45def6a89d8b
vrushbru <47853078+vrushbru@users.noreply.github.com>
2021-03-31 23:27:03 +0530

    Add files via upload

---
+++ Genkwiz-API/src/main/java/com/genkwiz/starter/Entity/SessionManagement.java
@@ -0,0 +1,83 @@
+package com.genkwiz.starter.Entity;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="session_management")
+public class SessionManagement {
+
+	@Id
+    @Column(name="id")
+	private UUID sessionId;
+	
+    @Column(name="user_name")
+	private String username;
+	
+    
+    @Column(name="avatar_id")
+	private int avatarId;
+	
+    
+    @Column(name="quiz_id")
+	private UUID quizId;
+
+    public SessionManagement() {
+    	
+    }
+    
+    
+	public SessionManagement(UUID sessionId, String username, int avatarId, UUID quizId) {
+		super();
+		this.sessionId = sessionId;
+		this.username = username;
+		this.avatarId = avatarId;
+		this.quizId = quizId;
+	}
+
+
+	public UUID getSessionId() {
+		return sessionId;
+	}
+
+
+	public void setSessionId(UUID sessionId) {
+		this.sessionId = sessionId;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public int getAvatarId() {
+		return avatarId;
+	}
+
+
+	public void setAvatarId(int avatarId) {
+		this.avatarId = avatarId;
+	}
+
+
+	public UUID getQuizId() {
+		return quizId;
+	}
+
+
+	public void setQuizId(UUID quizId) {
+		this.quizId = quizId;
+	}
+    
+    
+}
