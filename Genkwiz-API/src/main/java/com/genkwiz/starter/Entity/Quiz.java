f54c585b5a25ac041fc5439fe4276bebb82bb8b5
vrushbru <47853078+vrushbru@users.noreply.github.com>
2021-05-12 20:13:10 +0530

    Update Quiz.java

--- Genkwiz-API/src/main/java/com/genkwiz/starter/Entity/Quiz.java
+++ Genkwiz-API/src/main/java/com/genkwiz/starter/Entity/Quiz.java
@@ -1,12 +1,17 @@
 package com.genkwiz.starter.Entity;
 
 import java.sql.Time;
+import java.util.Set;
 import java.util.UUID;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.PrePersist;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.SecondaryTable;
 import javax.persistence.SecondaryTables;
@@ -26,6 +31,7 @@ public class Quiz {
 		
 		@Id
 		@Column(name="id")
+		
 		private UUID quizId;
 		
 		@Column(name="question_id")
@@ -43,21 +49,15 @@ public class Quiz {
 		@Column(name="is_attempted")
 		boolean isAttempted;
 		
-		@Transient
-		Question question;
-		@Transient
-		Hint hint;
-		@Transient
-		Mode mode;
-		@Transient
-		Multimedia multimedia;
+		@OneToMany(mappedBy="quiz")
+		private Set<Question> questions;
 		
 		public Quiz() {
 			
 		}
 		
 		public Quiz(UUID quizId, int qId,Time userTime, int numberofHints, int scorePerQuestion, boolean isAttempted,
-				Question question, Hint hint, Mode mode, Multimedia multimedia) {
+				Set<Question> question) {
 			super();
 			this.quizId = quizId;
 			this.qId=qId;
@@ -65,20 +65,22 @@ public class Quiz {
 			this.numberofHints = numberofHints;
 			this.scorePerQuestion = scorePerQuestion;
 			this.isAttempted = isAttempted;
-			this.question = question;
-			this.hint = hint;
-			this.mode = mode;
-			this.multimedia = multimedia;
+			this.questions = question;
+			
 		}
+		
+		
 
 		public UUID getQuizId() {
 			return quizId;
 		}
 
-		public void setQuizId(UUID quizId) {
+		
+		public void  setQuizId(UUID quizId) {
 			this.quizId = quizId;
 		}
 
+		
 		public Time getUserTime() {
 			return userTime;
 		}
@@ -111,36 +113,14 @@ public class Quiz {
 			this.isAttempted = isAttempted;
 		}
 
-		public Question getQuestion() {
-			return question;
+	
+
+		public Set<Question> getQuestion() {
+			return questions;
 		}
 
-		public void setQuestion(Question question) {
-			this.question = question;
-		}
-
-		public Hint getHint() {
-			return hint;
-		}
-
-		public void setHint(Hint hint) {
-			this.hint = hint;
-		}
-
-		public Mode getMode() {
-			return mode;
-		}
-
-		public void setMode(Mode mode) {
-			this.mode = mode;
-		}
-
-		public Multimedia getMultimedia() {
-			return multimedia;
-		}
-
-		public void setMultimedia(Multimedia multimedia) {
-			this.multimedia = multimedia;
+		public void setQuestion(Set<Question> question) {
+			this.questions = question;
 		}
 
 		public int getqId() {
@@ -151,6 +131,9 @@ public class Quiz {
 			this.qId = qId;
 		}
 		
-		
+		@Override
+		  public String toString() {
+		      return "Quiz [Quiz Id=" + this.quizId + ", usertime=" + this.userTime + ", QuestionId=" + this.qId + ", Attempted or not=" +this.isAttempted + "]";
+		  }
 		
 }
