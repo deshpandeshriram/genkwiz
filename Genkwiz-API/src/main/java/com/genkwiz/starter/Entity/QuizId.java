1d0df584d7a85153430cd418668cf68a6d8336bc
vrushbru <47853078+vrushbru@users.noreply.github.com>
2021-05-13 14:17:25 +0530

    Create QuizId.java

---
+++ Genkwiz-API/src/main/java/com/genkwiz/starter/Entity/QuizId.java
@@ -0,0 +1,22 @@
+package com.genkwiz.starter.Entity;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+public class QuizId implements Serializable {
+private UUID quizId;
+
+private int qId;
+
+QuizId(){
+	
+}
+
+public QuizId(UUID quizId, int qId) {
+	super();
+	this.quizId = quizId;
+	this.qId = qId;
+}
+	
+	
+}
