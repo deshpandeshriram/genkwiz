0c0b52af12d8cfccdbd8e4c173aeaa2059938194
vrushbru <47853078+vrushbru@users.noreply.github.com>
2021-05-13 14:18:18 +0530

    Update Quiz.java

--- Genkwiz-API/src/main/java/com/genkwiz/starter/Entity/Quiz.java
+++ Genkwiz-API/src/main/java/com/genkwiz/starter/Entity/Quiz.java
@@ -6,9 +6,12 @@ import java.util.UUID;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.PrePersist;
@@ -27,13 +30,14 @@ import javax.persistence.Transient;
 	@SecondaryTable(name="multimedia",pkJoinColumns=@PrimaryKeyJoinColumn(name="q_id"))
 	
 })*/
+@IdClass(QuizId.class)
 public class Quiz {
 		
 		@Id
-		@Column(name="id")
-		
+		@Column(name="id")		
 		private UUID quizId;
 		
+		@Id
 		@Column(name="question_id")
 		private int qId;
 		
@@ -46,26 +50,28 @@ public class Quiz {
 		@Column(name="score_per_question")
 		private int scorePerQuestion;
 		
-		@Column(name="is_attempted")
-		boolean isAttempted;
+
+	    @Enumerated(EnumType.STRING)
+		@Column(name="current_status")
+		Status status;
 		
-		@OneToMany(mappedBy="quiz")
-		private Set<Question> questions;
+		//@OneToOne(mappedBy="quiz")
+	    @OneToOne
+		private Question questions;
 		
 		public Quiz() {
 			
 		}
 		
-		public Quiz(UUID quizId, int qId,Time userTime, int numberofHints, int scorePerQuestion, boolean isAttempted,
-				Set<Question> question) {
+		public Quiz(UUID quizId, int qId,Time userTime, int numberofHints, int scorePerQuestion, Status status,Question questions) {
 			super();
 			this.quizId = quizId;
 			this.qId=qId;
 			this.userTime = userTime;
 			this.numberofHints = numberofHints;
 			this.scorePerQuestion = scorePerQuestion;
-			this.isAttempted = isAttempted;
-			this.questions = question;
+			this.status = status;
+			this.questions = questions;
 			
 		}
 		
@@ -105,22 +111,23 @@ public class Quiz {
 			this.scorePerQuestion = scorePerQuestion;
 		}
 
-		public boolean isAttempted() {
-			return isAttempted;
-		}
-
-		public void setAttempted(boolean isAttempted) {
-			this.isAttempted = isAttempted;
-		}
-
+		
 	
 
-		public Set<Question> getQuestion() {
+		public Status getStatus() {
+			return status;
+		}
+
+		public void setStatus(Status status) {
+			this.status = status;
+		}
+
+		public Question getQuestions() {
 			return questions;
 		}
 
-		public void setQuestion(Set<Question> question) {
-			this.questions = question;
+		public void setQuestions(Question questions) {
+			this.questions = questions;
 		}
 
 		public int getqId() {
@@ -133,7 +140,7 @@ public class Quiz {
 		
 		@Override
 		  public String toString() {
-		      return "Quiz [Quiz Id=" + this.quizId + ", usertime=" + this.userTime + ", QuestionId=" + this.qId + ", Attempted or not=" +this.isAttempted + "]";
+		      return "Quiz [Quiz Id=" + this.quizId + ", usertime=" + this.userTime + ", QuestionId=" + this.qId + ", Attempted or not=" +this.status + "]";
 		  }
 		
 }
